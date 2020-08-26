aa3cd7062675c93a012bfdfd10ddc4886bc0b325
vadpunj <vad.conan@gmail.com>
2020-08-26 15:17:39 +0700

    add admin

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -11,9 +11,10 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
+      DB::table('users')->truncate();
       DB::table('users')->insert([
         [
-          'name' => 'phatsirin',
+          'name' => 'Phatsirin Srisaengchai',
           'emp_id' => '01000583',
           'type' => 1,
           'center_money' => '1N00203',
@@ -22,7 +23,7 @@ class AdminTableSeeder extends Seeder
           'updated_at' => date('Y-m-d H:i:s')
         ],
         [
-          'name' => 'dumkerng',
+          'name' => 'Dumkerng Muikeaw',
           'emp_id' => '00368195',
           'type' => 1,
           'center_money' => '1N00203',
@@ -31,8 +32,44 @@ class AdminTableSeeder extends Seeder
           'updated_at' => date('Y-m-d H:i:s')
         ],
         [
-          'name' => 'panicha',
+          'name' => 'Panicha Saenkhueansi',
           'emp_id' => '01000554',
+          'type' => 1,
+          'center_money' => '1N00203',
+          'user_id' => '01000583',
+          'created_at' => date('Y-m-d H:i:s'),
+          'updated_at' => date('Y-m-d H:i:s')
+        ],
+        [
+          'name' => 'Wanida Chomthamai',
+          'emp_id' => '01000103',
+          'type' => 1,
+          'center_money' => '1N00203',
+          'user_id' => '01000583',
+          'created_at' => date('Y-m-d H:i:s'),
+          'updated_at' => date('Y-m-d H:i:s')
+        ],
+        [
+          'name' => 'Prapatsorn Prechan',
+          'emp_id' => '00309374',
+          'type' => 1,
+          'center_money' => '1N00203',
+          'user_id' => '01000583',
+          'created_at' => date('Y-m-d H:i:s'),
+          'updated_at' => date('Y-m-d H:i:s')
+        ],
+        [
+          'name' => 'Jariya Eamapichat',
+          'emp_id' => '00232739',
+          'type' => 1,
+          'center_money' => '1N00203',
+          'user_id' => '01000583',
+          'created_at' => date('Y-m-d H:i:s'),
+          'updated_at' => date('Y-m-d H:i:s')
+        ],
+        [
+          'name' => 'Punyabha Auparikchatpong',
+          'emp_id' => '01000363',
           'type' => 1,
           'center_money' => '1N00203',
           'user_id' => '01000583',
